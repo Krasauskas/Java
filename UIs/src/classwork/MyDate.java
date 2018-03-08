f92b63c5bc16f01db3716c53a5b6093b1d237864
__V <krasauskas.vytautas@gmail.com>
2018-03-08 10:43:27 +0000

    Finished MyDate exercise

--- UIs/src/classwork/MyDate.java
+++ UIs/src/classwork/MyDate.java
@@ -11,25 +11,36 @@ import java.util.Date;
 public class MyDate {
 
 	int year, month, day;
+	DateFormat y = new SimpleDateFormat("yyyy");
+	DateFormat m = new SimpleDateFormat("MM");
+	DateFormat d = new SimpleDateFormat("dd");
 	
 	public MyDate() {
-		DateFormat y = new SimpleDateFormat("yyyy");
-		DateFormat m = new SimpleDateFormat("MM");
-		DateFormat d = new SimpleDateFormat("dd");
+		
 		Date date = new Date(System.currentTimeMillis());
 		year = Integer.parseInt(y.format(date));
 		month = Integer.parseInt(m.format(date));
 		day = Integer.parseInt(d.format(date));
 	}
 	
-	public MyDate(long elapsedTime) {
-		DateFormat y = new SimpleDateFormat("yyyy");
-		DateFormat m = new SimpleDateFormat("MM");
-		DateFormat d = new SimpleDateFormat("dd");
-		Date date = new Date(elapsedTime);
+	public MyDate(long time) {
+		Date date = new Date(time);
 		year = Integer.parseInt(y.format(date));
 		month = Integer.parseInt(m.format(date));
 		day = Integer.parseInt(d.format(date));
+	}
+	
+	public MyDate(int year, int month, int day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	
+	public void setDate(long elapsedTime) {
+		Date date = new Date(elapsedTime);
+		this.year = year + Integer.parseInt(y.format(date));
+		this.month = month + Integer.parseInt(m.format(date));
+		this.day = day + Integer.parseInt(d.format(date));
 	}
 	
 	public int getYear() {
@@ -46,9 +57,14 @@ public class MyDate {
 	
 	public static void main(String[] args) {
 		
-		MyDate test = new MyDate(561555550000L);
-		System.out.println(test.getYear());
-		System.out.println(test.getMonth());
+		MyDate test = new MyDate();
+		System.out.print(test.getYear() + " ");
+		System.out.print(test.getMonth() + " ");
+		System.out.println(test.getDay());
+		
+		MyDate test2 = new MyDate(34355555133101L);
+		System.out.print(test2.getYear() + " ");
+		System.out.print(test2.getMonth() + " ");
 		System.out.println(test.getDay());
 	}
 
